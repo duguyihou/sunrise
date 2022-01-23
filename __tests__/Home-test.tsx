35c466347931fb5fffd98ed9c3a45f2ae2151506
duguyihou <duguyihou@gmail.com>
2022-01-23 19:30:48 +1100

    feat: add typings

--- __tests__/Home-test.tsx
+++ __tests__/Home-test.tsx
@@ -4,10 +4,60 @@
 
 import 'react-native'
 // Note: test renderer must be required after react-native.
-import eventReducer from 'app/eventsSlice'
+import eventReducer from 'app/calendarEventsSlice'
 test('should return the initial state', () => {
   expect(eventReducer(undefined, { type: String, payload: String })).toEqual([
-    'a',
-    'b',
+    {
+      id: '1231j1nunjn',
+      summary: 'Google I/O 2015',
+      location: '800 Howard St., San Francisco, CA 94103',
+      description: "A chance to hear more about Google's developer products.",
+      start: {
+        dateTime: '2015-05-28T09:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      end: {
+        dateTime: '2015-05-28T17:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+      attendees: [
+        { email: 'lpage@example.com' },
+        { email: 'sbrin@example.com' },
+      ],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: 'email', minutes: 24 * 60 },
+          { method: 'popup', minutes: 10 },
+        ],
+      },
+    },
+    {
+      id: 'asjidj8q81ji12i',
+      summary: 'Google I/O 2016',
+      location: '800 Howard St., San Francisco, CA 94103',
+      description: "A chance to hear more about Google's developer products.",
+      start: {
+        dateTime: '2015-05-28T09:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      end: {
+        dateTime: '2015-05-28T17:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+      attendees: [
+        { email: 'lpage@example.com' },
+        { email: 'sbrin@example.com' },
+      ],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: 'email', minutes: 24 * 60 },
+          { method: 'popup', minutes: 10 },
+        ],
+      },
+    },
   ])
 })
