ffeb3b6ebc9c6995f7c76a6c80d8da1d0b060fc4
duguyihou <duguyihou@gmail.com>
2022-02-05 12:34:22 +1100

    chore: change auth scope

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,6 +3,6 @@ import Config from 'react-native-config'
 export const config = {
   issuer: 'https://accounts.google.com',
   clientId: `${Config.GOOGLE_OAUTH_APP_GUID}.apps.googleusercontent.com`,
-  redirectUrl: `com.googleusercontent.apps.${Config.GOOGLE_OAUTH_APP_GUID}:/oauth2redirect/google`,
-  scopes: ['openid', 'profile'],
+  redirectUrl: `com.googleusercontent.apps.${Config.GOOGLE_OAUTH_APP_GUID}:/oauth2redirect/tasks/`,
+  scopes: ['openid', 'https://www.googleapis.com/auth/tasks'],
 }
