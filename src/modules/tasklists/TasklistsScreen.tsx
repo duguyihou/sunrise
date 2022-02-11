3cf59f0e503a7079ee444dd926b2e800686ed846
duguyihou <duguyihou@gmail.com>
2022-02-12 08:55:35 +1100

    refactor: add home route

--- src/modules/tasklists/TasklistsScreen.tsx
+++ src/modules/tasklists/TasklistsScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useLayoutEffect } from 'react'
 import { useQuery } from 'react-query'
 import { ScrollView, StyleSheet, Text } from 'react-native'
-import { faListUl } from '@fortawesome/free-solid-svg-icons'
+import { faCalendar, faListUl } from '@fortawesome/free-solid-svg-icons'
 
 import { RootStackParamList, StackProps, TasklistQuery } from 'typings'
 import { windowHeight, windowWidth } from 'utils/dimensions'
 import TasklistItem from 'components/TasklistItem'
 import { theme, routeNames } from 'shared'
 import tasklistService from 'api/tasklists'
+import TasklistHome from 'components/TasklistHome'
 
 const TasklistsScreen = ({ navigation, route }: StackProps) => {
   useLayoutEffect(() => navigation.navigate(routeNames.Home))
@@ -15,13 +16,18 @@ const TasklistsScreen = ({ navigation, route }: StackProps) => {
     'tasklists',
     async () => await tasklistService.findAll(),
   )
+  const allTasklists = data?.items
   if (isLoading) return <Text>loading...</Text>
   if (error) return <Text>`An error has occurred: ${error.message}`</Text>
 
-  const allTasklists = data?.items
-
   return (
     <ScrollView style={styles.container}>
+      <TasklistHome
+        path={routeNames.Home}
+        icon={faCalendar}
+        navigation={navigation}
+        route={route}
+      />
       {allTasklists &&
         allTasklists.map(tasklist => (
           <TasklistItem
