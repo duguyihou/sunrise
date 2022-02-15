6bc96a76e93c2f215b6b2b4bebf2f4f104a1966a
duguyihou <duguyihou@gmail.com>
2022-02-15 19:08:36 +1100

    feat: add checkbox

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,15 +1,23 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Task } from 'typings'
 import { windowWidth } from 'utils/dimensions'
+import Checkbox from './Checkbox'
 
 type Props = {
   task: Task
 }
 const TaskItem = ({ task }: Props) => {
   const { title } = task
+  const [isChecked, setIsChecked] = useState(false)
+  const handleCheck = () => {
+    setIsChecked(!isChecked)
+  }
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => console.log('🐵 touch')}>
+      <Checkbox onPress={handleCheck} isChecked={false} />
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   )
@@ -19,6 +27,7 @@ export default TaskItem
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     width: windowWidth,
     padding: 10,
   },
