f6f99259ebb5e79bf2d8cb177a4632eb827aa418
duguyihou <duguyihou@gmail.com>
2022-03-05 22:05:37 +1100

    feat(taskItem): add due & notes

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,34 +1,38 @@
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { StackNavigationProps, Task } from 'typings'
-import { windowWidth } from 'utils/dimensions'
 import Checkbox from './Checkbox'
 import { useUpdateTaskMutation } from 'hooks/tasks'
-import { RouteName, TaskStatus, theme } from 'shared'
+import { RouteName, TaskStatus } from 'shared'
 import { useNavigation } from '@react-navigation/native'
+import { windowWidth } from 'utils/dimensions'
+import TaskItemDue from './TaskItemDue'
+import TaskItemNotes from './TaskItemNotes'
+import TaskItemTitle from './TaskItemTitle'
 
 type Props = {
   task: Task
 }
 const TaskItem = ({ task }: Props) => {
-  const { title, selfLink, status } = task
+  const { title, selfLink, status, due, notes } = task
   const navigation = useNavigation<StackNavigationProps>()
   const isChecked = status === TaskStatus.Completed
-
   const updateTaskStatusMutation = useUpdateTaskMutation(selfLink, {
     ...task,
     status: !isChecked ? TaskStatus.Completed : TaskStatus.NeedsAction,
   })
   const handleCheck = () => updateTaskStatusMutation.mutate()
-
   return (
     <TouchableOpacity
+      activeOpacity={1}
       style={styles.container}
       onPress={() => navigation.push(RouteName.TaskDetail, { selfLink })}>
       <Checkbox isChecked={isChecked} onPress={handleCheck} />
-      <Text style={isChecked ? styles.completedTitle : styles.needsActionTitle}>
-        {title}
-      </Text>
+      <View style={styles.task}>
+        {title && <TaskItemTitle isChecked={isChecked} title={title} />}
+        {notes && <TaskItemNotes notes={notes} />}
+        {due && <TaskItemDue due={due} />}
+      </View>
     </TouchableOpacity>
   )
 }
@@ -37,16 +41,17 @@ export default TaskItem
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
     width: windowWidth,
-    padding: 10,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-  needsActionTitle: {
-    fontSize: 20,
-  },
-  completedTitle: {
-    fontSize: 20,
-    color: theme.font.placeholder,
-    textDecorationLine: 'line-through',
+  task: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingRight: 40, // TODO: not sure why need 40 padding
   },
 })
---
+++ src/components/TaskItemDue.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from 'react-native'
+import React from 'react'
+import { getCalendar } from 'utils/dateTime'
+
+type Props = { due: Date }
+const TaskItemDue = ({ due }: Props) => {
+  return <Text style={styles.due}>{getCalendar(due)}</Text>
+}
+
+export default TaskItemDue
+
+const styles = StyleSheet.create({
+  due: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderRadius: 15,
+  },
+})
