6b8169cefd5815831b2abc063dc24fefbcf013ef
duguyihou <duguyihou@gmail.com>
2022-03-15 20:23:45 +1100

    refactor: date time component

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,43 +1,37 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import IconButton from './IconButton'
-import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
 import { RouteName, theme } from 'shared'
 import { StackNavigationProps } from 'typings'
 import { getCalendar } from 'utils/dateTime'
 import { useNavigation } from '@react-navigation/native'
-import { useAppDispatch, useAppSelector } from 'app/hooks'
+import { useAppDispatch } from 'app/hooks'
 import { clearTask } from 'app/tasks'
 
 type Props = {
-  iconClickable: boolean
+  dateTime: string
 }
-const TaskDateTime = ({ iconClickable }: Props) => {
-  const navigation = useNavigation<StackNavigationProps>()
-  const {
-    newTask: { due },
-  } = useAppSelector(state => state.tasks)
-  const dispatch = useAppDispatch()
 
-  const handleSetDate = () =>
-    iconClickable && navigation.navigate(RouteName.DateTime)
+const DateTime = ({ dateTime }: Props) => {
+  const navigation = useNavigation<StackNavigationProps>()
+  const dispatch = useAppDispatch()
+  dateTime = new Date().toISOString()
+  const handleSetDate = () => navigation.navigate(RouteName.DateTime)
   const handleRemove = () => dispatch(clearTask())
   return (
     <View style={styles.container}>
-      {!due && <IconButton icon={faCalendarCheck} fn={handleSetDate} />}
-      {!!due && (
-        <TouchableOpacity style={styles.dateTime} onPress={handleSetDate}>
-          <Text>{due ? getCalendar(due) : 'Add date/time'}</Text>
+      <TouchableOpacity style={styles.dateTime} onPress={handleSetDate}>
+        <Text>{dateTime ? getCalendar(dateTime) : 'Add date/time'}</Text>
+        {!!dateTime && (
           <TouchableOpacity onPress={handleRemove}>
             <Text style={styles.remove}>X</Text>
           </TouchableOpacity>
-        </TouchableOpacity>
-      )}
+        )}
+      </TouchableOpacity>
     </View>
   )
 }
 
-export default TaskDateTime
+export default DateTime
 
 const styles = StyleSheet.create({
   container: {
