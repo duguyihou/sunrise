f0215838c13a4ed7697b392a3494747de167aa8c
duguyihou <duguyihou@gmail.com>
2022-03-19 20:59:11 +1100

    feat: remove notes icon

--- src/components/TaskNotes.tsx
+++ src/components/TaskNotes.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { faAlignLeft } from '@fortawesome/free-solid-svg-icons'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { StyleSheet, TextInput } from 'react-native'
 import { useAppDispatch, useAppSelector } from 'app/hooks'
 import { theme } from 'shared'
-import IconButton from './IconButton'
 import { updateTask } from 'app/tasks'
 
 type Props = {
@@ -15,33 +13,26 @@ const TaskNotes = ({ notes }: Props) => {
   const handleOnChange = (text: string) =>
     dispatch(updateTask({ ...task, notes: text }))
   return (
-    <View style={styles.container}>
-      <IconButton icon={faAlignLeft} />
-      <TextInput
-        multiline
-        style={styles.notes}
-        value={notes}
-        onChangeText={handleOnChange}
-        placeholder="Add Notes"
-        blurOnSubmit={false}
-      />
-    </View>
+    <TextInput
+      multiline
+      style={styles.notes}
+      value={notes}
+      onChangeText={handleOnChange}
+      placeholder="Add Notes"
+      blurOnSubmit={false}
+    />
   )
 }
 
 export default TaskNotes
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    paddingTop: 10,
-    flexDirection: 'row',
-  },
   notes: {
     flex: 1,
-    paddingTop: 0,
-    paddingHorizontal: 10,
+    padding: 10,
     fontSize: 16,
     color: theme.font.placeholder,
+    minHeight: 150,
+    backgroundColor: theme.bg.secondary,
   },
 })
