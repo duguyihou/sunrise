25155365e957d8eecdcb9d7e3b1bb0c50c61930e
duguyihou <duguyihou@gmail.com>
2022-04-08 21:20:40 +1000

    fix: show different background when editing tasklist title

--- src/modules/TasklistScreen/components/HeaderTitle.tsx
+++ src/modules/TasklistScreen/components/HeaderTitle.tsx
@@ -2,6 +2,7 @@ import { useTasklists } from 'hooks/app'
 import { useUpdateTasklistMutation } from 'hooks/tasklists'
 import React, { useState } from 'react'
 import { StyleSheet, TextInput } from 'react-native'
+import { theme } from 'shared/theme'
 
 function HeaderTitle() {
   const {
@@ -11,7 +12,7 @@ function HeaderTitle() {
   const [value, setValue] = useState(title)
   const updateTasklistMutation = useUpdateTasklistMutation(id, value)
   const handleOnBlur = () => {
-    updateTasklistMutation.mutate()
+    if (value !== title) updateTasklistMutation.mutate()
     setEditable(false)
   }
   const handlePress = () => setEditable(true)
@@ -19,7 +20,7 @@ function HeaderTitle() {
   return (
     <TextInput
       editable={editable}
-      style={styles.textInput}
+      style={[styles.input, editable && styles.editableInput]}
       value={value}
       onPressIn={handlePress}
       onChangeText={setValue}
@@ -31,9 +32,13 @@ function HeaderTitle() {
 export default HeaderTitle
 
 const styles = StyleSheet.create({
-  textInput: {
-    // width: '60%',
+  input: {
+    width: '60%',
     fontSize: 20,
     textAlign: 'center',
+    padding: 5,
+  },
+  editableInput: {
+    backgroundColor: theme.bg.secondary,
   },
 })
